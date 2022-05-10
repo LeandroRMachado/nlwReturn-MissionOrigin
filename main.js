96811ef1238c575e7ad2daf2ac4a1d88a87943d0
LeandroRMachado <leandroskullofwar@outlook.com>
2022-05-10 20:21:56 -0300

    add backToTopButton and styles

--- main.js
+++ main.js
@@ -1,11 +1,37 @@
 //JavaScript
+window.addEventListener('scroll', onScroll)
 
+onScroll()
 function onScroll() {
+  showNavOnScroll()
+  showBackToTopButtonOnScroll()
+  changeColorOnScrollButton()
+}
+
+function showNavOnScroll() {
   const scroll = scrollY
   if (scroll > 0) {
     navigation.classList.add('scroll')
   } else {
     navigation.classList.remove('scroll')
+  }
+}
+
+function showBackToTopButtonOnScroll() {
+  const scroll = scrollY
+  if (scroll > 390) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+function changeColorOnScrollButton() {
+  const scroll = scrollY
+  if (scroll > 4260) {
+    backToTopButton.classList.add('changeColorButtonScroll')
+  } else {
+    backToTopButton.classList.remove('changeColorButtonScroll')
   }
 }
 
@@ -31,4 +57,7 @@ ScrollReveal({
 #about,
 #about header,
 #about .content,
-#about .content img`)
+#about .content img,
+#contact,
+#contact header,
+#contact .content`)
